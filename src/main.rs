582fc99b4f86c1f69b86373ce04a4f7a0356a15a
John Weachock <jweachock@gmail.com>
2021-03-30 21:04:38 -0400

    Spit out dot code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use pest_derive::Parser;
 use std::fs;
 use std::io::prelude::*;
 
+// you're about to read some awful, hacky, PoC Rust code.
+// you've been warned.
+
 #[derive(Parser)]
 #[grammar = "recipe.pest"]
 struct RecipeParser;
@@ -14,6 +17,11 @@ fn main() {
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     let mut steps = RecipeParser::parse(Rule::main, &contents).unwrap();
+    let mut ing_count = 0;
+
+    println!("digraph {{");
+    println!("  rankdir=LR;");
+
     for step in steps {
         if step.as_rule() == Rule::EOI {
             break;
@@ -22,7 +30,7 @@ fn main() {
         let mut inner = step.into_inner();
         let id = inner.next().map(|x| x.as_str().to_string()).unwrap();
         let action = inner.next().map(|x| x.as_str().to_string()).unwrap();
-        println!("{} {}", id, action);
+        println!("  step{}[shape=cds, label=\"{}\", style=filled, fillcolor=\"#FFFF99\"];", id, action);
 
         let ings = inner.next().unwrap().into_inner();
         for ing in ings {
@@ -30,13 +38,17 @@ fn main() {
             let inner = ing.into_inner().next().unwrap();
             match inner.as_rule() {
                 Rule::raw_ingredient => {
-                    dbg!(&inner);
+                    ing_count += 1;
+                    println!("  ing{}[shape=box, label=\"{}\", style=filled, fillcolor=\"#9999FF\"];", ing_count, label);
+                    println!("  ing{} -> step{}", ing_count, id);
                 }
                 Rule::prev_ingredient => {
-                    println!("=> #{}", inner.into_inner().as_str().to_string());
+                    println!("  step{} -> step{}", inner.into_inner().as_str().to_string(), id);
                 }
                 _ => panic!("fuc"),
             }
         }
     }
+
+    println!("}}");
 }
