afcad4b361664412ca3c0a56ab320351c70c4732
John Weachock <jweachock@gmail.com>
2021-03-30 20:57:59 -0400

    Basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use pest::iterators::Pair;
-use std::io::prelude::*;
-use std::fs;
 use pest::iterators::Pairs;
 use pest::Parser;
 use pest_derive::Parser;
+use std::fs;
+use std::io::prelude::*;
 
 #[derive(Parser)]
 #[grammar = "recipe.pest"]
@@ -13,6 +13,30 @@ fn main() {
     let mut file = fs::File::open("recipe").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let mut main = RecipeParser::parse(Rule::main, &contents);
-    dbg!(main);
+    let mut steps = RecipeParser::parse(Rule::main, &contents).unwrap();
+    for step in steps {
+        if step.as_rule() == Rule::EOI {
+            break;
+        }
+
+        let mut inner = step.into_inner();
+        let id = inner.next().map(|x| x.as_str().to_string()).unwrap();
+        let action = inner.next().map(|x| x.as_str().to_string()).unwrap();
+        println!("{} {}", id, action);
+
+        let ings = inner.next().unwrap().into_inner();
+        for ing in ings {
+            let label = ing.as_str().to_string();
+            let inner = ing.into_inner().next().unwrap();
+            match inner.as_rule() {
+                Rule::raw_ingredient => {
+                    dbg!(&inner);
+                }
+                Rule::prev_ingredient => {
+                    println!("=> #{}", inner.into_inner().as_str().to_string());
+                }
+                _ => panic!("fuc"),
+            }
+        }
+    }
 }
